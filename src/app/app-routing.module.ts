a8def16c0de173e1c4d9d98c88cc74fc1d92dfb2
mouheb ben chiekh <benchiekhmouheb@gmail.com>
2022-01-31 23:49:24 +0100

    front product list and add product

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddProductComponent } from './add-product/add-product.component';
+import { ListProductComponent } from './list-product/list-product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'addProduct', component: AddProductComponent },
+  { path: 'products', component: ListProductComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NbCardModule, NbInputModule, NbThemeModule } from '@nebular/theme';
+import {
+  NbSidebarModule,
+  NbLayoutModule,
+  NbButtonModule,
+  NbSearchModule,
+} from '@nebular/theme';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { ListProductComponent } from './list-product/list-product.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, AddProductComponent, ListProductComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NbThemeModule.forRoot(),
+    NbLayoutModule,
+    NbSidebarModule.forRoot(), // NbSidebarModule.forRoot(), //if this is your app.module
+    NbButtonModule,
+    NbSearchModule,
+    NbCardModule,
+    NbInputModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
